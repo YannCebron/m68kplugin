c5e8c57a99f82921c13eec86842cf8f0935f15e5
Yann Cébron <yann.cebron@jetbrains.com>
2020-04-11 18:40:18 +0200

    highlighting: enable unresolved ref highlighting via registry key only

--- src/com/yanncebron/m68kplugin/lang/highlighting/M68kSyntaxAnnotator.java
+++ src/com/yanncebron/m68kplugin/lang/highlighting/M68kSyntaxAnnotator.java
@@ -19,6 +19,7 @@ package com.yanncebron.m68kplugin.lang.highlighting;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
+import com.intellij.openapi.util.registry.Registry;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
 import com.yanncebron.m68kplugin.lang.psi.M68kLabel;
@@ -38,7 +39,7 @@ public class M68kSyntaxAnnotator implements Annotator {
     }
 
     // todo temp --> inspection?
-    if (element instanceof M68kLabelReference) {
+    if (element instanceof M68kLabelReference && Registry.is("m68k.highlight.unresolved.ref")) {
       final PsiReference reference = element.getReference();
       if (reference.resolve() == null) {
         holder.createErrorAnnotation(element, ProblemsHolder.unresolvedReferenceMessage(reference));
