c21bbdd2745de5f07e5c38f5dc9d028eccd8c761
Yann Cébron <yann.cebron@jetbrains.com>
2020-05-01 17:53:22 +0200

    extract ConditionalAssemblyDirectivesParsingTest

---
+++ tests/com/yanncebron/m68kplugin/parser/ConditionalAssemblyDirectivesParsingTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2020 The Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.yanncebron.m68kplugin.parser;
+
+import com.intellij.testFramework.TestDataPath;
+
+@TestDataPath("$PROJECT_ROOT/testData/parser/directives/conditionalAssembly")
+public class ConditionalAssemblyDirectivesParsingTest extends M68kParsingTestCase {
+
+  public ConditionalAssemblyDirectivesParsingTest() {
+    super("directives/conditionalAssembly");
+  }
+
+  public void testIfdDirective() throws Exception {
+    doCodeTest(" ifd NAME");
+  }
+
+  public void testIfdDirectiveMissingLabel() throws Exception {
+    doCodeTest(" ifd ");
+  }
+
+  public void testIfeqDirective() throws Exception {
+    doCodeTest(" ifeq 1+2");
+  }
+
+  public void testIfneDirective() throws Exception {
+    doCodeTest(" ifne 1+2");
+  }
+
+  public void testIfgeDirective() throws Exception {
+    doCodeTest(" ifge 1+2");
+  }
+
+  public void testIfgtDirective() throws Exception {
+    doCodeTest(" ifgt 1+2");
+  }
+
+  public void testIfleDirective() throws Exception {
+    doCodeTest(" ifle 1+2");
+  }
+
+  public void testIfltDirective() throws Exception {
+    doCodeTest(" iflt 1+2");
+  }
+
+  public void testIfndDirective() throws Exception {
+    doCodeTest(" ifnd NAME");
+  }
+
+  public void testElseDirective() throws Exception {
+    doCodeTest(" else");
+  }
+
+  public void testElseifDirective() throws Exception {
+    doCodeTest(" elseif");
+  }
+
+  public void testEndcDirective() throws Exception {
+    doCodeTest(" endc");
+  }
+
+}
