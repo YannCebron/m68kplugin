5506bc4a22865679918ff5cc934e8306e5ea1c7c
Yann Cébron <yann.cebron@jetbrains.com>
2020-06-19 16:14:07 +0200

    brace matching: all conditional assembly directives

--- src/com/yanncebron/m68kplugin/lang/M68kBraceMatcher.java
+++ src/com/yanncebron/m68kplugin/lang/M68kBraceMatcher.java
@@ -29,8 +29,22 @@ public class M68kBraceMatcher implements PairedBraceMatcher {
   private final BracePair[] pairs = new BracePair[]{
     new BracePair(M68kTokenTypes.L_PAREN, M68kTokenTypes.R_PAREN, false),
     new BracePair(M68kTokenTypes.L_BRACKET, M68kTokenTypes.R_BRACKET, false),
+
     new BracePair(M68kTokenTypes.MACRO, M68kTokenTypes.ENDM, true),
-    new BracePair(M68kTokenTypes.IFND, M68kTokenTypes.ENDC, true)
+
+    new BracePair(M68kTokenTypes.IF, M68kTokenTypes.ENDC, true),
+    new BracePair(M68kTokenTypes.IFB, M68kTokenTypes.ENDC, true),
+    new BracePair(M68kTokenTypes.IFNB, M68kTokenTypes.ENDC, true),
+    new BracePair(M68kTokenTypes.IFD, M68kTokenTypes.ENDC, true),
+    new BracePair(M68kTokenTypes.IFND, M68kTokenTypes.ENDC, true),
+    new BracePair(M68kTokenTypes.IFEQ, M68kTokenTypes.ENDC, true),
+    new BracePair(M68kTokenTypes.IFGE, M68kTokenTypes.ENDC, true),
+    new BracePair(M68kTokenTypes.IFGT, M68kTokenTypes.ENDC, true),
+    new BracePair(M68kTokenTypes.IFLE, M68kTokenTypes.ENDC, true),
+    new BracePair(M68kTokenTypes.IFLT, M68kTokenTypes.ENDC, true),
+    new BracePair(M68kTokenTypes.IFNE, M68kTokenTypes.ENDC, true),
+    new BracePair(M68kTokenTypes.ELSE, M68kTokenTypes.ENDC, true),
+    new BracePair(M68kTokenTypes.ELSEIF, M68kTokenTypes.ENDC, true)
   };
 
   @NotNull
