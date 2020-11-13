9ff9b4c67101938384b4666ff88b25ec586a694e
Yann Cébron <yann.cebron@jetbrains.com>
2020-11-13 12:02:48 +0100

    M68kLabelResolveTest.testHighlightResolvingInSingleFile: test weak highlighting for directives

--- testData/resolve/label/highlightResolvingInSingleFile.s
+++ testData/resolve/label/highlightResolvingInSingleFile.s
@@ -11,3 +11,17 @@ topLevelLabel
   bra <error descr="Cannot resolve symbol 'INVALID_LABEL'">INVALID_LABEL</error>
   bra <error descr="Cannot resolve symbol '.INVALID_LABEL'">.INVALID_LABEL</error>
 
+
+* directives referring potentially undefined label - weak highlighting
+
+  ifd <weak_warning descr="Cannot resolve symbol 'INVALID_VALUE'">INVALID_VALUE</weak_warning>
+  endif
+
+  ifnd <weak_warning descr="Cannot resolve symbol 'INVALID_VALUE'">INVALID_VALUE</weak_warning>
+  endif
+
+  ifmacrod <weak_warning descr="Cannot resolve symbol 'INVALID_VALUE'">INVALID_VALUE</weak_warning>
+  endif
+
+  ifmacrond <weak_warning descr="Cannot resolve symbol 'INVALID_VALUE'">INVALID_VALUE</weak_warning>
+  endif
