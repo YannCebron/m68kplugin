7769111eb7000263a450a53d27728faf0419982b
Yann Cébron <yann.cebron@jetbrains.com>
2022-03-31 10:38:27 +0200

    gradle-intellij-plugin 1.5.2

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -25,5 +25,5 @@ repositories {
 
 dependencies {
     // Gradle IntelliJ Plugin
-    implementation("org.jetbrains.intellij.plugins", "gradle-intellij-plugin", "1.5.1")
+    implementation("org.jetbrains.intellij.plugins", "gradle-intellij-plugin", "1.5.2")
 }
