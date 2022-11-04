3814f1b9b594c5ad9fbeac19542598d7c529d9ac
Yann Cébron <yann.cebron@jetbrains.com>
2022-11-04 12:21:51 +0100

    build: org.jetbrains.changelog 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 The Authors
+ * Copyright 2022 The Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import org.jetbrains.changelog.Changelog
 import org.jetbrains.grammarkit.tasks.GenerateLexerTask
 import org.jetbrains.intellij.tasks.RunPluginVerifierTask.FailureLevel
 
@@ -113,9 +114,9 @@ tasks {
 
         // Get the latest available change notes from the changelog file
         changeNotes.set(provider {
-            changelog.run {
+            changelog.renderItem(changelog.run {
                 getOrNull(properties("pluginVersion")) ?: getLatest()
-            }.toHTML()
+            }, Changelog.OutputType.HTML)
         })
     }
 
