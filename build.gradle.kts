7fb68759c67e34abd22ef7022f06c98b37df7a66
Yann Cébron <yann.cebron@jetbrains.com>
2023-03-04 11:49:13 +0100

    build: use instrumentedJar for submodules

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 The Authors
+ * Copyright 2023 The Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,8 +31,8 @@ plugins {
 group = properties("pluginGroup")
 
 dependencies {
-    implementation(project("amiga"))
-    implementation(project("plugin-api"))
+    implementation(project("amiga", "instrumentedJar"))
+    implementation(project("plugin-api", "instrumentedJar"))
 }
 
 // Configure project's dependencies
@@ -102,7 +102,7 @@ tasks {
         enabled = true
     }
 
-     verifyPluginConfiguration {
+    verifyPluginConfiguration {
         enabled = true
     }
 
