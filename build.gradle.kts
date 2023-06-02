b1c1432ec148e682c7a724e03c08de17bfffd2e9
Yann Cébron <yann.cebron@jetbrains.com>
2023-06-02 15:59:19 +0200

    [build] org.jetbrains.changelog 2.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ plugins {
     // generic Java setup
     id("m68kplugin.java-conventions")
     // Gradle Changelog Plugin
-    id("org.jetbrains.changelog") version "2.0.0"
+    id("org.jetbrains.changelog") version "2.1.0"
     // GrammarKit
     id("org.jetbrains.grammarkit") version "2021.2.2"
 }
