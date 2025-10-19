62d1c33f4eaccdbe9169313762715c982ca9ad61
Yann Cébron <yann.cebron@jetbrains.com>
2025-10-19 15:16:59 +0200

    add M68kDirectiveDocumentationProviderTest.testDirectivesWithoutReferenceDoc()

--- tests/com/yanncebron/m68kplugin/documentation/M68kDirectiveDocumentationProviderTest.java
+++ tests/com/yanncebron/m68kplugin/documentation/M68kDirectiveDocumentationProviderTest.java
@@ -19,13 +19,66 @@ package com.yanncebron.m68kplugin.documentation;
 import com.intellij.codeInsight.documentation.DocumentationManager;
 import com.intellij.lang.documentation.DocumentationProvider;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.tree.TokenSet;
 import com.intellij.testFramework.fixtures.BasePlatformTestCase;
 import com.intellij.util.PairConsumer;
+import com.yanncebron.m68kplugin.lang.psi.M68kTokenGroups;
 import com.yanncebron.m68kplugin.lang.psi.M68kTokenTypes;
 import com.yanncebron.m68kplugin.lang.psi.directive.M68kSoDirective;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @SuppressWarnings("SpellCheckingInspection")
 public class M68kDirectiveDocumentationProviderTest extends BasePlatformTestCase {
+
+  public void testDirectivesWithoutReferenceDoc() {
+    assertNoReferenceDoc(M68kTokenGroups.DIRECTIVES,
+      M68kTokenTypes.AC68080,
+      M68kTokenTypes.ADDWATCH,
+      M68kTokenTypes.AUTO,
+      M68kTokenTypes.CPU32,
+      M68kTokenTypes.DS,
+      M68kTokenTypes.DSEG,
+      M68kTokenTypes.EQUR,
+      M68kTokenTypes.FAR,
+      M68kTokenTypes.FPU,
+      M68kTokenTypes.INITNEAR,
+      M68kTokenTypes.JUMPERR,
+      M68kTokenTypes.JUMPPTR,
+      M68kTokenTypes.MACHINE,
+      M68kTokenTypes.MASK2,
+      M68kTokenTypes.MC68000,
+      M68kTokenTypes.MC68010,
+      M68kTokenTypes.MC68020,
+      M68kTokenTypes.MC68030,
+      M68kTokenTypes.MC68040,
+      M68kTokenTypes.MC68060,
+      M68kTokenTypes.NEAR,
+      M68kTokenTypes.NEAR_CODE,
+      M68kTokenTypes.OPT,
+      M68kTokenTypes.REG
+    );
+
+    assertNoReferenceDoc(M68kTokenGroups.CONDITIONAL_ASSEMBLY_DIRECTIVES,
+      M68kTokenTypes.ELSEIF,
+      M68kTokenTypes.ENDC,
+      M68kTokenTypes.IFMI,
+      M68kTokenTypes.IFPL
+    );
+  }
+
+  private void assertNoReferenceDoc(TokenSet tokenSet, IElementType... missing) {
+    Set<IElementType> noReferenceDoc = new HashSet<>();
+    for (IElementType directive : tokenSet.getTypes()) {
+      String referenceDoc = M68kDirectiveDocumentationProvider.getDirectiveDoc(directive);
+      if (referenceDoc.contains(M68kDocumentationUtil.CONTRIBUTION_FOOTER)) {
+        noReferenceDoc.add(directive);
+      }
+    }
+    assertSameElements(noReferenceDoc, missing);
+  }
 
   public void testMacroCallDirectiveNoReferenceDocAvailable() {
     doTest(" macro<caret>Name param", (psiElement, documentationProvider) -> {
@@ -34,7 +87,7 @@ public class M68kDirectiveDocumentationProviderTest extends BasePlatformTestCase
     });
   }
 
-  public void testMc68000DirectiveNoReferenceDocAvailable() {
+  public void testDirectiveNoReferenceDocAvailableFallback() {
     doTest(" mc<caret>68000", (psiElement, documentationProvider) -> {
       String doc = documentationProvider.generateDoc(psiElement, getOriginalElement());
       assertEquals("<style>h1 { font-weight: bold; font-size: 120%; } h2 { padding-top: 13px; font-weight: bold; font-size: 110%; } h3 { padding-top: 10px; font-weight: bold; } table { padding-bottom: 10px; white-space: nowrap; } td { margin: 4px 0 0 0; padding: 0 0 0 0; }th { font-weight: bold; text-align: left; white-space: nowrap; margin: 2px; } em { font-style: italic; }code { white-space: nowrap; }p { padding-top: 5px; } strong { font-weight: bold; }blockquote { padding-left: 10px; padding-right:10px; padding-bottom: 5px; }</style><h1>MC68000</h1><p>No reference documentation available for 'mc68000'</p><br><br><a href=\"http://sun.hasenbraten.de/vasm/release/vasm_4.html#Directives-2\">vasm directives docs 1</a><br><br><a href=\"http://sun.hasenbraten.de/vasm/release/vasm_23.html#Extensions-3\">vasm directives docs 2</a><br><br><a href=\"https://github.com/prb28/m68k-instructions-documentation\">Contribute to m68k-instructions-documentation project</a>", doc);
