4887b72b15ef8afd6859e1cb1b6d0dec3c3ba1f7
Yann Cébron <yann.cebron@jetbrains.com>
2025-11-09 14:55:38 +0100

    M68kOperand: add vasm codes in javadoc

--- src/com/yanncebron/m68kplugin/lang/psi/M68kOperand.java
+++ src/com/yanncebron/m68kplugin/lang/psi/M68kOperand.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 The Authors
+ * Copyright 2025 The Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,14 +21,32 @@ package com.yanncebron.m68kplugin.lang.psi;
  */
 public enum M68kOperand {
 
+  /**
+   * vasm: {@code 0}
+   */
   NONE,
 
+  /**
+   * vasm: {@code IM}
+   */
   IMMEDIATE(M68kAddressMode.IMMEDIATE),
+  /**
+   * vasm: {@code QI}
+   */
   QUICK_IMMEDIATE(M68kAddressMode.QUICK_IMMEDIATE),
 
+  /**
+   * vasm: {@code BR}
+   */
   BRANCH_DESTINATION(M68kAddressMode.LABEL),
+  /**
+   * vasm: {@code DB}
+   */
   DBCC_BRANCH_DESTINATION(M68kAddressMode.LABEL),
 
+  /**
+   * vasm: {@code CT}
+   */
   CONTROL(M68kAddressMode.ADDRESS_REGISTER_INDIRECT,
     M68kAddressMode.ADDRESS_REGISTER_DISPLACEMENT,
     M68kAddressMode.ADDRESS_REGISTER_INDEX_DISPLACEMENT,
@@ -36,12 +54,18 @@ public enum M68kOperand {
     M68kAddressMode.ABSOLUTE_LONG,
     M68kAddressMode.PC_REGISTER_DISPLACEMENT,
     M68kAddressMode.PC_REGISTER_INDEX_DISPLACEMENT),
+  /**
+   * vasm: {@code AC}
+   */
   ALTERABLE_CONTROL(M68kAddressMode.ADDRESS_REGISTER_INDIRECT,
     M68kAddressMode.ADDRESS_REGISTER_DISPLACEMENT,
     M68kAddressMode.ADDRESS_REGISTER_INDEX_DISPLACEMENT,
     M68kAddressMode.ABSOLUTE_SHORT,
     M68kAddressMode.ABSOLUTE_LONG),
 
+  /**
+   * vasm: {@code MR}
+   */
   RESTORE_OPERANDS(M68kAddressMode.ADDRESS_REGISTER_INDIRECT,
     M68kAddressMode.ADDRESS_REGISTER_INDIRECT_POST_INCREMENT,
     M68kAddressMode.ADDRESS_REGISTER_DISPLACEMENT,
@@ -50,8 +74,14 @@ public enum M68kOperand {
     M68kAddressMode.ABSOLUTE_LONG,
     M68kAddressMode.PC_REGISTER_DISPLACEMENT,
     M68kAddressMode.PC_REGISTER_INDEX_DISPLACEMENT),
+  /**
+   * vasm: {@code IR}
+   */
   IMMEDIATE_REGISTER_LIST_VALUE(M68kAddressMode.IMMEDIATE),
 
+  /**
+   * vasm: {@code AY}
+   */
   ALL(M68kAddressMode.DATA_REGISTER,
     M68kAddressMode.ADDRESS_REGISTER,
     M68kAddressMode.ADDRESS_REGISTER_INDIRECT,
@@ -64,6 +94,9 @@ public enum M68kOperand {
     M68kAddressMode.PC_REGISTER_DISPLACEMENT,
     M68kAddressMode.PC_REGISTER_INDEX_DISPLACEMENT,
     M68kAddressMode.IMMEDIATE),
+  /**
+   * vasm: {@code DA}
+   */
   DATA(M68kAddressMode.DATA_REGISTER,
     M68kAddressMode.IMMEDIATE,
     M68kAddressMode.ADDRESS_REGISTER_INDIRECT,
@@ -75,6 +108,9 @@ public enum M68kOperand {
     M68kAddressMode.ABSOLUTE_LONG,
     M68kAddressMode.PC_REGISTER_DISPLACEMENT,
     M68kAddressMode.PC_REGISTER_INDEX_DISPLACEMENT),
+  /**
+   * vasm: {@code MA}
+   */
   MEMORY(M68kAddressMode.ADDRESS_REGISTER_INDIRECT,
     M68kAddressMode.ADDRESS_REGISTER_INDIRECT_POST_INCREMENT,
     M68kAddressMode.ADDRESS_REGISTER_INDIRECT_PRE_DECREMENT,
@@ -85,6 +121,9 @@ public enum M68kOperand {
     M68kAddressMode.PC_REGISTER_DISPLACEMENT,
     M68kAddressMode.PC_REGISTER_INDEX_DISPLACEMENT,
     M68kAddressMode.IMMEDIATE),
+  /**
+   * vasm: {@code MI}
+   */
   MEMORY_WITHOUT_IMMEDIATE(M68kAddressMode.ADDRESS_REGISTER_INDIRECT,
     M68kAddressMode.ADDRESS_REGISTER_INDIRECT_POST_INCREMENT,
     M68kAddressMode.ADDRESS_REGISTER_INDIRECT_PRE_DECREMENT,
@@ -95,6 +134,9 @@ public enum M68kOperand {
     M68kAddressMode.PC_REGISTER_DISPLACEMENT,
     M68kAddressMode.PC_REGISTER_INDEX_DISPLACEMENT),
 
+  /**
+   * vasm: {@code AL}
+   */
   ALTERABLE(M68kAddressMode.DATA_REGISTER,
     M68kAddressMode.ADDRESS_REGISTER,
     M68kAddressMode.ADDRESS_REGISTER_INDIRECT,
@@ -104,6 +146,9 @@ public enum M68kOperand {
     M68kAddressMode.ADDRESS_REGISTER_INDEX_DISPLACEMENT,
     M68kAddressMode.ABSOLUTE_SHORT,
     M68kAddressMode.ABSOLUTE_LONG),
+  /**
+   * vasm: {@code AD}
+   */
   ALTERABLE_DATA(M68kAddressMode.DATA_REGISTER,
     M68kAddressMode.ADDRESS_REGISTER_INDIRECT,
     M68kAddressMode.ADDRESS_REGISTER_INDIRECT_POST_INCREMENT,
@@ -112,6 +157,9 @@ public enum M68kOperand {
     M68kAddressMode.ADDRESS_REGISTER_INDEX_DISPLACEMENT,
     M68kAddressMode.ABSOLUTE_SHORT,
     M68kAddressMode.ABSOLUTE_LONG),
+  /**
+   * vasm: {@code CFAD}
+   */
   ALTERABLE_DATA_CF(M68kAddressMode.DATA_REGISTER,
     M68kAddressMode.ADDRESS_REGISTER_INDIRECT,
     M68kAddressMode.ADDRESS_REGISTER_INDIRECT_POST_INCREMENT,
@@ -120,6 +168,9 @@ public enum M68kOperand {
     M68kAddressMode.ADDRESS_REGISTER_INDEX_DISPLACEMENT,
     M68kAddressMode.ABSOLUTE_SHORT,
     M68kAddressMode.ABSOLUTE_LONG),
+  /**
+   * vasm: {@code AM}
+   */
   ALTERABLE_MEMORY(M68kAddressMode.ADDRESS_REGISTER_INDIRECT,
     M68kAddressMode.ADDRESS_REGISTER_INDIRECT_POST_INCREMENT,
     M68kAddressMode.ADDRESS_REGISTER_INDIRECT_PRE_DECREMENT,
@@ -127,6 +178,9 @@ public enum M68kOperand {
     M68kAddressMode.ADDRESS_REGISTER_INDEX_DISPLACEMENT,
     M68kAddressMode.ABSOLUTE_SHORT,
     M68kAddressMode.ABSOLUTE_LONG),
+  /**
+   * vasm: {@code CFAM}
+   */
   ALTERABLE_MEMORY_CF(M68kAddressMode.ADDRESS_REGISTER_INDIRECT,
     M68kAddressMode.ADDRESS_REGISTER_INDIRECT_POST_INCREMENT,
     M68kAddressMode.ADDRESS_REGISTER_INDIRECT_PRE_DECREMENT,
@@ -135,15 +189,39 @@ public enum M68kOperand {
     M68kAddressMode.ABSOLUTE_SHORT,
     M68kAddressMode.ABSOLUTE_LONG),
 
+  /**
+   * vasm: {@code D_}
+   */
   DATA_REGISTER(M68kAddressMode.DATA_REGISTER),
+  /**
+   * vasm: {@code A_}
+   */
   ADDRESS_REGISTER(M68kAddressMode.ADDRESS_REGISTER),
 
+  /**
+   * vasm: {@code R_}
+   */
   DATA_OR_ADDRESS_REGISTER(M68kAddressMode.DATA_REGISTER, M68kAddressMode.ADDRESS_REGISTER),
+  /**
+   * vasm: {@code RL}
+   */
   DATA_OR_ADDRESS_REGISTER_LIST(M68kAddressMode.REGISTER_LIST),
 
+  /**
+   * vasm: {@code PA}
+   */
   ADDRESS_REGISTER_INDIRECT_PRE_DECREMENT(M68kAddressMode.ADDRESS_REGISTER_INDIRECT_PRE_DECREMENT),
+  /**
+   * vasm: {@code AP}
+   */
   ADDRESS_REGISTER_INDIRECT_POST_INCREMENT(M68kAddressMode.ADDRESS_REGISTER_INDIRECT_POST_INCREMENT),
+  /**
+   * vasm: {@code DP}
+   */
   ADDRESS_REGISTER_DISPLACEMENT(M68kAddressMode.ADDRESS_REGISTER_DISPLACEMENT),
+  /**
+   * vasm: {@code ???}
+   */
   ADDRESS_REGISTER_INDEX_DISPLACEMENT(M68kAddressMode.ADDRESS_REGISTER_INDEX_DISPLACEMENT),
 
   SR_REGISTER(M68kAddressMode.SPECIAL_REGISTER_SR),
