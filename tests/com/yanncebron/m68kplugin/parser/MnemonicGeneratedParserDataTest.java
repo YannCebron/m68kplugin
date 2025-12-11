3535f6b5840eec25fb7ef766b8574cb574786204
Yann Cébron <yann.cebron@jetbrains.com>
2025-12-11 17:58:26 +0100

    add MnemonicGeneratedParserDataTest.testParserDoesNotAllowInvalidAddressModes() to validate parser is not too lenient

--- tests/com/yanncebron/m68kplugin/parser/MnemonicGeneratedParserDataTest.java
+++ tests/com/yanncebron/m68kplugin/parser/MnemonicGeneratedParserDataTest.java
@@ -16,9 +16,14 @@
 
 package com.yanncebron.m68kplugin.parser;
 
+import com.intellij.openapi.util.Ref;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.PsiErrorElement;
+import com.intellij.psi.PsiRecursiveElementVisitor;
 import com.intellij.psi.tree.IElementType;
 import com.intellij.util.SmartList;
+import com.intellij.util.containers.ContainerUtil;
+import com.intellij.util.containers.FactoryMap;
 import com.yanncebron.m68kplugin.lang.psi.*;
 import com.yanncebron.m68kplugin.lang.psi.directive.M68kMacroCallDirective;
 import org.jetbrains.annotations.NotNull;
@@ -28,14 +33,6 @@ import java.util.*;
 
 import static java.util.Map.entry;
 
-/**
- * Generate all possible unique variants for all registered {@link M68kMnemonic} and verify:
- * <ul>
- * <li>parsing yields no errors</li>
- * <li>mnemonic is recognized (not a macro call)</li>
- * <li>every variant returns entry via {@link M68kMnemonicRegistry#find}</li>
- * </ul>
- */
 public class MnemonicGeneratedParserDataTest extends M68kParsingTestCase {
 
   // testData/sanity/AllInstructionsParsing.s
@@ -45,7 +42,7 @@ public class MnemonicGeneratedParserDataTest extends M68kParsingTestCase {
   private static final boolean SKIP_DUPLICATE_VARIANTS = true;
 
   private static final String INDENT = "         ";
-  private int labelCount = 0;
+  private int labelCounter = 0;
   private int total = 0, deprecated = 0;
   private final List<String> failedVariants = new SmartList<>();
 
@@ -57,7 +54,7 @@ public class MnemonicGeneratedParserDataTest extends M68kParsingTestCase {
   protected void setUp() throws Exception {
     super.setUp();
 
-    labelCount = 0;
+    labelCounter = 0;
     total = 0;
     deprecated = 0;
     failedVariants.clear();
@@ -76,6 +73,123 @@ public class MnemonicGeneratedParserDataTest extends M68kParsingTestCase {
     if (DUMP) System.out.println(text);
   }
 
+  /**
+   * Generate all _invalid_ address mode variants and make sure they result in a parser error or not found mnemonic.
+   * This avoids "too lenient" parser (BNF).
+   */
+  public void testParserDoesNotAllowInvalidAddressModes() {
+    int matchedByMnemonicCount = 0;
+    int skippedValidCount = 0;
+
+    for (IElementType instructionType : M68kTokenGroups.INSTRUCTIONS.getTypes()) {
+//    for (IElementType instructionType : TokenSet.create(M68kTokenTypes.ABCD).getTypes()) {
+
+      Collection<M68kMnemonic> allMnemonics = M68kMnemonicRegistry.getInstance().findAll(instructionType);
+      assertNotEmpty(allMnemonics);
+
+      boolean hasSourceOperand = ContainerUtil.exists(allMnemonics, m68kMnemonic -> m68kMnemonic.sourceOperand() != M68kOperand.NONE);
+      boolean hasDestinationOperand = ContainerUtil.exists(allMnemonics, m68kMnemonic -> m68kMnemonic.destinationOperand() != M68kOperand.NONE);
+      if (!hasSourceOperand && !hasDestinationOperand) {
+        continue;
+      }
+      boolean hasDataSize = ContainerUtil.exists(allMnemonics, m68kMnemonic -> !m68kMnemonic.dataSizes().contains(M68kDataSize.UNSIZED));
+
+      Set<String> generatedVariants = new HashSet<>();
+      for (M68kDataSize dataSize : M68kDataSize.values()) {
+        if (!hasDataSize && dataSize != M68kDataSize.UNSIZED) continue; // UNSIZED mnemonic with size becomes macro call
+
+        String dataSizeText = dataSize == M68kDataSize.UNSIZED ? "" : dataSize.getText();
+
+        for (M68kAddressMode sourceAdm : M68kAddressMode.values()) {
+          for (M68kAddressMode destinationAdm : M68kAddressMode.values()) {
+
+            boolean foundValid = false;
+            for (M68kMnemonic known : allMnemonics) {
+              if ((dataSize == M68kDataSize.UNSIZED || known.dataSizes().contains(dataSize)) &&
+                containsAddressMode(known.sourceOperand(), sourceAdm) &&
+                (known.destinationOperand() == M68kOperand.NONE || containsAddressMode(known.destinationOperand(), destinationAdm))) {
+                foundValid = true;
+                break;
+              }
+            }
+            if (foundValid) {
+              skippedValidCount++;
+              continue;
+            }
+
+            // enough to test against first notation variant
+            String sourceText = ADDRESS_MODE_TEXT.get(sourceAdm).get(0);
+            String variant;
+            if (hasDestinationOperand) {
+              String destinationText = ADDRESS_MODE_TEXT.get(destinationAdm).get(0);
+              variant = "  " + instructionType + dataSizeText + " " + sourceText + "," + destinationText;
+            } else {
+              variant = "  " + instructionType + dataSizeText + " " + sourceText;
+            }
+            if (!generatedVariants.add(variant)) {
+              continue; // skip duplicates from unnecessary loop (hasDestinationOperand=false)
+            }
+
+            String variantOutput = variant + StringUtil.repeat(" ", 30 - variant.length()) + " ; " + sourceAdm + (hasDestinationOperand ? "," + destinationAdm : "") + " ";
+            total++;
+            myFile = createPsiFile("a", variant);
+            M68kPsiElement m68kPsiElement = M68kPsiTreeUtil.getContainingInstructionOrDirective(myFile.findElementAt(3));
+            ensureNoMacroCallElements();
+            M68kInstruction m68kInstruction = assertInstanceOf(m68kPsiElement, M68kInstruction.class);
+
+            Ref<Boolean> foundAnyError = Ref.create(Boolean.FALSE);
+            myFile.accept(new PsiRecursiveElementVisitor() {
+              @Override
+              public void visitErrorElement(@NotNull PsiErrorElement element) {
+                foundAnyError.set(true);
+                super.visitErrorElement(element);
+              }
+            });
+            if (foundAnyError.get()) {
+              dump(variantOutput + "failed expectedly");
+            } else {
+              // check that adm's match if no parser error (IMMEDIATE vs QUICK_IMMEDIATE, LABEL vs ABSOLUTE etc.)
+              try {
+                M68kMnemonic foundMnemonic = M68kMnemonicRegistry.getInstance().find(m68kInstruction);
+                if (foundMnemonic != null) {
+                  matchedByMnemonicCount++;
+                  dump(variantOutput + "no parser error, but found matching mnemonic: " + foundMnemonic);
+                  continue;
+                }
+              } catch (AssertionError expected) {
+                // no matching mnemonic currently yields assertion
+              }
+
+              failedVariants.add(variantOutput);
+              dump(variantOutput + "SHOULD HAVE FAILED");
+            }
+
+          }
+        }
+      }
+    }
+
+    assertEquals(121291, total);
+    assertEquals(17166, skippedValidCount);
+    assertEquals(1472, matchedByMnemonicCount);
+    assertEmpty(failedVariants);
+  }
+
+  private static boolean containsAddressMode(M68kOperand m68kOperand, M68kAddressMode addressMode) {
+    for (M68kAddressMode mode : m68kOperand.getAddressModes()) {
+      if (mode == addressMode) return true;
+    }
+    return false;
+  }
+
+  /**
+   * Generate all possible _valid_ unique variants for all registered {@link M68kMnemonic} and verify:
+   * <ul>
+   * <li>parsing yields no errors</li>
+   * <li>mnemonic is recognized (not a macro call)</li>
+   * <li>every variant returns entry via {@link M68kMnemonicRegistry#find}</li>
+   * </ul>
+   */
   public void testGenerateAndParseAllInstructions() {
     for (IElementType instructionsType : M68kTokenGroups.INSTRUCTIONS.getTypes()) {
       doParserTest(instructionsType);
@@ -126,7 +240,7 @@ public class MnemonicGeneratedParserDataTest extends M68kParsingTestCase {
     final M68kInstruction m68kInstruction = assertInstanceOf(instruction, M68kInstruction.class);
 
     final M68kMnemonic m68kMnemonic = M68kMnemonicRegistry.getInstance().find(m68kInstruction);
-    assertNotNull(m68kMnemonic);
+    assertNotNull(myFile.getText(), m68kMnemonic);
   }
 
   private void ensureNoMacroCallElements() {
@@ -134,7 +248,7 @@ public class MnemonicGeneratedParserDataTest extends M68kParsingTestCase {
 
       @Override
       public void visitMacroCallDirective(@NotNull M68kMacroCallDirective o) {
-        fail("unexpected macrocall '" + o.getText() + "'");
+        fail("unexpected macrocall '" + myFile.getText() + "'");
       }
     });
   }
@@ -154,15 +268,15 @@ public class MnemonicGeneratedParserDataTest extends M68kParsingTestCase {
       String labelName = "<NOT_BRANCH>";
       String labelOrIndent = INDENT;
       if (needsLocalBranchLabel) {
-        labelName = "label" + String.format("%02d", labelCount++);
+        labelName = "label" + String.format("%02d", labelCounter++);
         labelOrIndent = labelName + ": ";
       }
 
       String mnemonicText = mnemonic.elementType().toString();
       List<String> variants = new SmartList<>();
 
-      for (String sourceText : getOperandTexts(mnemonic.sourceOperand())) {
-        for (String destinationText : getOperandTexts(mnemonic.destinationOperand())) {
+      for (String sourceText : OPERAND_TEXTS.get(mnemonic.sourceOperand())) {
+        for (String destinationText : OPERAND_TEXTS.get(mnemonic.destinationOperand())) {
           if (mnemonic.dataSizes().iterator().next() != M68kDataSize.UNSIZED) {
             addVariant(variants, labelOrIndent, mnemonicText, needsLocalBranchLabel ? labelName : sourceText, destinationText, "  ");
           }
@@ -216,22 +330,23 @@ public class MnemonicGeneratedParserDataTest extends M68kParsingTestCase {
     entry(M68kAddressMode.LABEL, List.of("label")),
     entry(M68kAddressMode.IMMEDIATE, List.of("#42")),
     entry(M68kAddressMode.QUICK_IMMEDIATE, List.of("#1")),
-    entry(M68kAddressMode.REGISTER_LIST, List.of("d0/a0-a2", "#1")),
+    entry(M68kAddressMode.REGISTER_LIST, List.of("d0/a0-a2", "#3")),
     entry(M68kAddressMode.SPECIAL_REGISTER_SR, List.of("SR")),
     entry(M68kAddressMode.SPECIAL_REGISTER_USP, List.of("USP")),
     entry(M68kAddressMode.SPECIAL_REGISTER_CCR, List.of("CCR")),
     entry(M68kAddressMode.CONTROL_REGISTER, List.of("DFC", "SFC", "VBR"))
   );
 
-  @NotNull
-  private static Collection<String> getOperandTexts(M68kOperand operand) {
-    if (operand == M68kOperand.NONE) return Collections.singleton(null);
+  private static final Map<M68kOperand, List<String>> OPERAND_TEXTS = FactoryMap.create(operand -> {
+    if (operand == M68kOperand.NONE) {
+      return Collections.singletonList(null);
+    }
 
     List<String> texts = new SmartList<>();
     for (M68kAddressMode addressMode : operand.getAddressModes()) {
-      assertTrue(addressMode.toString(), ADDRESS_MODE_TEXT.containsKey(addressMode));
       texts.addAll(ADDRESS_MODE_TEXT.get(addressMode));
     }
     return texts;
-  }
+  });
+
 }
