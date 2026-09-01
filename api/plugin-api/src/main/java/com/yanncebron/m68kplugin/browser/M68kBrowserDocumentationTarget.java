7e88dfdd13940dbdb174edc71932a264c361a3ac
Yann Cébron <yann.cebron@jetbrains.com>
2026-09-01 19:39:04 +0200

    M68kBrowserDocumentationTarget: fix link in doc

--- api/plugin-api/src/main/java/com/yanncebron/m68kplugin/browser/M68kBrowserDocumentationTarget.java
+++ api/plugin-api/src/main/java/com/yanncebron/m68kplugin/browser/M68kBrowserDocumentationTarget.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 The Authors
+ * Copyright 2026 The Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@ import com.intellij.platform.backend.presentation.TargetPresentation;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * @see M68kBrowserItemDocumentationLinkHandler
+ * @see M68kBrowserDocumentationLinkHandler
  */
 @SuppressWarnings("UnstableApiUsage")
 abstract class M68kBrowserDocumentationTarget implements DocumentationTarget {
